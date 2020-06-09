4cd7d82cb73202e770993eec17716d95cf0af435
AleksandrTherzyan <50660660+AleksandrTherzyan@users.noreply.github.com>
2020-06-09 17:26:46 +0300

    Update modifier.resize.php

--- api/smarty-plugins/modifier.resize.php
+++ api/smarty-plugins/modifier.resize.php
@@ -5,6 +5,9 @@ use Api\Simpla;
 function smarty_modifier_resize($filename, $width=0, $height=0, $set_watermark=false)
 {
     $simpla = new Simpla();
+    if ($set_watermark == 'w') {
+        $set_watermark = true;
+    }
 
     return $simpla->image->resize_image($filename, $width, $height, $set_watermark);
 }
